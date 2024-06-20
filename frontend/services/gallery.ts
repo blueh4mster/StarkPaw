db6e43852e557cfdd6e1abe476df6bcf17843b8c
preeti <karnwalpreeti102@gmail.com>
2024-06-20 23:39:21 +0530

    added dummy funcs

--- frontend/services/gallery.ts
+++ frontend/services/gallery.ts
@@ -8,17 +8,7 @@ export const fetchPets = async() => {
     // fetch pet nft address owned by this address from backend 
     // return array of these addresses
     let array : Array<pets> = [];
-    let pet1 : pets = {
-        nftAddr: "0x123",
-        tracker: "cat",
-        uri : "/"
-    }
-    let pet2 : pets = {
-        nftAddr: "0x456",
-        tracker: "dog",
-        uri : "/"
-    }
-    array.push(pet1);
-    array.push(pet2);
+    // fill array by fetching details from backend and making interface 
     return array;
 }
+
