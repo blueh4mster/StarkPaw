470aa7092a0a4680283bca2f0b14f2e492c10a5b
preeti <karnwalpreeti102@gmail.com>
2024-06-21 20:56:47 +0530

    added fetch function

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,13 +5,45 @@ import {
 import Head from "next/head";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
+import { Account, Chain, Hex, Transport, WalletClient } from "viem";
+import { useState } from "react";
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import { Call, Contract } from "starknet";
 import PetCard from "../components/PetCard";
 import { petChosen } from "@/services/petCard";
 import Sidebar from "@/components/Sidebar";
+import { abi2 } from "../abi";
 
 export default function Home() {
-  const handleClick = (pet: string) => {
-    petChosen(pet);
+  const { primaryWallet } = useDynamicContext();
+  const contractAddr = "";
+  const [txnHash, setTxnHash] = useState("");
+
+  if (!primaryWallet) return null;
+
+  const handleClick = async (pet: string) => {
+    const provider = await primaryWallet.connector.getSigner<
+      WalletClient<Transport, Chain, Account>
+    >();
+    if (!provider) return;
+
+    const nftContract = new Contract(
+      abi2.abi,contractAddr,
+      provider as any
+    );
+    const mintCallData: Call = nftContract.populate(
+      "_mint",
+      {
+        recipient: provider.account.address,
+        tokenId: 1
+      }
+    );
+    const { transaction_hash: transferTxHash } =
+      await nftContract._mint(mintCallData.calldata);
+
+    const hash = await provider.waitForTransaction(transferTxHash);
+
+    setTxnHash(hash.transaction_hash);
   };
   const links = [
     { href: "/", label: "Home" },
@@ -31,7 +63,10 @@ export default function Home() {
           theme={"dark"}
           settings={{
             environmentId: "4ab7a405-d8b1-4fe7-97bd-7c6ead2e8f66",
-            walletConnectors: [EthereumWalletConnectors, StarknetWalletConnectors],
+            walletConnectors: [
+              EthereumWalletConnectors,
+              StarknetWalletConnectors,
+            ],
           }}
         >
           <DynamicWidget />
